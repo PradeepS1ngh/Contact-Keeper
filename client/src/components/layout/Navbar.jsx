61fb4cb2b46bb560d5b8571edd89217f50ef745b
Pradeep <pradeepsingh17101998@gmail.com>
2020-12-27 14:09:35 +0530

    Navbar And Route Setup

---
+++ client/src/components/layout/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return ( 
+        <nav className='navbar navbar-expand-lg navbar-dark bg-primary'>
+            <div class="container-fluid">
+            <Link to='/' className='navbar-brand'>Contact-Keeper</Link>
+            <ul className='navbar-nav'>
+                <li className='nav-item'>
+                    <Link to='/' className='nav-link active'>Home</Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to='/about' className='nav-link'>About</Link>
+                </li>
+            </ul>
+            </div>
+        </nav>
+    );
+}
+export default Navbar;
---
+++ client/src/components/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+    return ( 
+        <h1> HomE</h1>
+    );
+}
+
+export default Home;
