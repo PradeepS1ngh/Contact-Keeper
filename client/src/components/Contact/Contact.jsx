f584db5c092db32c3e84e806e8a463a84f49bbbf
Pradeep <pradeepsingh17101998@gmail.com>
2020-12-28 12:54:20 +0530

    React Transition

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -2,23 +2,30 @@ import React, { useContext } from 'react'
 import ContactContext from '../../Context/Contacts/ContactContext'
 import ContactItem from './ContactItem'
 
+//Transition
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
+
 
 const Contact = () => {
     const contactContext = useContext(ContactContext);
     const { contacts, filtered } = contactContext;
 
-    if(contacts.length == 0){
+    if (contacts.length == 0) {
         return <h4 className='text-center'>Please Add a Contact</h4>
     }
     return (
-        <> {filtered !== null ?
+        <TransitionGroup> {filtered !== null ?
             filtered.map((contact) => {
-                return <ContactItem key={contact.id} contact={contact} />
+                return <CSSTransition key={contact.id} timeout={500} classNames="alert" >
+                            <ContactItem  contact={contact} />
+                        </CSSTransition>
             }) : contacts.map((contact) => {
-                return <ContactItem key={contact.id} contact={contact} />
+                return <CSSTransition key={contact.id} timeout={500} classNames="alert" >
+                            <ContactItem  contact={contact} />
+                        </CSSTransition>
             })}
-
-        </>
+        </TransitionGroup>
     )
 }
 
