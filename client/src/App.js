f099a41b2b4a9f1ec30a0862ba5af7d7c6317209
Pradeep <pradeepsingh17101998@gmail.com>
2020-12-28 13:35:12 +0530

    Auth Context Setup

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import { Switch, Route } from 'react-router-dom';
 
 // Context
 import ContactState from './Context/Contacts/ContactState'
+import AuthState from './Context/Auth/AuthState'
 
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
@@ -16,17 +17,20 @@ import About from './components/pages/About';
 
 function App() {
   return (
-    <ContactState>
-      <Router>
-        <Navbar />
-        <div className='container' style={{maxWidth:"1307px"}}>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/about' component={About} />
-          </Switch>
-        </div>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Navbar />
+          <div className='container' style={{ maxWidth: "1307px" }}>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
+        </Router>
+      </ContactState>
+    </AuthState>
+
   );
 }
 
