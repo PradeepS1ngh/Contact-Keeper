ccb68ac7f8742264f0634362ef2be1a5c0ce91c2
Pradeep <pradeepsingh17101998@gmail.com>
2020-12-30 13:07:36 +0530

    prepare fro Deploy

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
 const express = require("express");
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
+const path = require("path");
 const app = express();
-
 
 //  MongoDB Connection
 connectDB();
 
 // Init Middlewares for req.body
-app.use(express.json({extended : false}));
-
-
-app.get("/", (req, res) => {
-    res.json({ msg: "welcome to Contact Keeper API" });
-});
+app.use(express.json({ extended: false }));
 
 //?  Defines Routes
-app.use('/api/user', require("./routes/user"));
-app.use('/api/auth', require("./routes/auth"));
-app.use('/api/contacts', require("./routes/contact"));
+app.use("/api/user", require("./routes/user"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contact"));
+
+//serve static assets in production
+if (process.env.NODE_ENV === "production") {
+    //set Static folder
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) =>
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    );
+}
 
 let PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}`);
 });
-
